3c2cc6f5408dcf34b2d25e088e4710ca1506a63f
Simply-divine <hardikvu0204@gmail.com>
2023-07-23 21:18:30 +0530

    Refactor: Remove Redundant styles

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
-import './carouselStyles.css';
+import './Carousel.css';
 
 const CarouselCard = () => {
   return (
